3e177aeac35aadd254faa9dd2eead3dbab85bb3c
Ben Ward <bw@ben-ward.net>
2017-12-17 22:55:49 +0000

    Significantly better Haskell solution to day 1

---
+++ src/AocHaskell/Day01/InverseCaptcha.hs
@@ -0,0 +1,11 @@
+import System.IO (readFile)
+import Data.Char (digitToInt)
+
+inverseCaptcha :: String -> (Int -> Int) -> Int
+inverseCaptcha input checkPosition = sum . map fst $ filter (\(x, i) -> x == (cycle digits) !! checkPosition i) (zip digits [0..])
+    where digits = map digitToInt input
+
+main :: IO ()
+main = do   puzzleInput <- readFile "../data/01"
+            print $ inverseCaptcha puzzleInput succ
+            print $ inverseCaptcha puzzleInput (\i -> i + div (length puzzleInput) 2)
