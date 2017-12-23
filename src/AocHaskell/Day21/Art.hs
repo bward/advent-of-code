411d277dd49968388aa21873420361fe21edce72
Ben Ward <Ben.Ward@softwire.com>
2017-12-23 21:31:43 +0000

    Rewrite Haskell solution to day 21 to use vectors, improving performance ~7x

--- src/AocHaskell/Day21/Art.hs
+++ src/AocHaskell/Day21/Art.hs
@@ -1,40 +1,46 @@
-import System.IO (readFile)
-import Data.List (find, transpose)
+import Data.List (transpose)
 import Data.List.Split (splitOn)
+import qualified Data.Map.Strict as M
+import Data.Foldable (asum)
+import qualified Data.Vector as V
 
-data Pixel = On | Off deriving (Eq)
-type Picture = [[Pixel]]
-type Rule = (Picture, Picture)
+data Pixel = On | Off deriving (Eq, Ord)
+type Picture = V.Vector (V.Vector Pixel)
+type Rules = M.Map Picture Picture
 
-disassemble :: Picture -> [Picture]
+vecTranspose :: V.Vector (V.Vector a) -> V.Vector (V.Vector a)
+vecTranspose = V.fromList . map V.fromList . transpose . V.toList . V.map V.toList
+
+disassemble :: Picture -> V.Vector Picture
 disassemble picture =
-    [[(take size) . (drop j) $ picture !! k | k <- [i..i+size-1]] | i <- range, j <- [0, size .. length picture - 1]]
+    V.fromList [V.map (V.slice j size) . (V.slice i size) $ picture | i <- range, j <- range]
     where
         size = if length picture `mod` 2 == 0 then 2 else 3
         range = [0, size.. length picture - 1]
 
-assemble :: [Picture] -> Picture
+assemble :: V.Vector Picture -> Picture
 assemble parts = 
-    concat [map concat . transpose . (take size) . (drop j) $ parts | j <- [0, size.. length parts - 1]]
-    where size = floor . sqrt . fromIntegral . length $ parts
+    V.concat [V.map V.concat . V.map V.toList . vecTranspose . (V.slice j size) $ parts | j <- [0, size.. length parts - 1]]
+    where size = floor . sqrt . fromIntegral . V.length $ parts
 
-enhance :: [Rule] -> Picture -> Picture
+enhance :: Rules -> Picture -> Picture
 enhance rules picture
-    | length picture > 3 = assemble . map (enhance rules) . disassemble $ picture
-    | otherwise = snd $ case find ((`elem` transforms picture) . fst ) rules of Just x -> x
-    where transforms picture = concat . take 4 . iterate (map rotate) $ [picture, flip picture]
+    | length picture > 3 = assemble . V.map (enhance rules) . disassemble $ picture
+    | otherwise = case asum . map (\p -> M.lookup p rules) $ transforms picture of Just x -> x
+    where transforms picture = concat . take 4 . iterate (map rotate) $ [picture, reflect picture]
             where
-                flip = map reverse
-                rotate = flip . transpose
+                reflect = V.map V.reverse
+                rotate = reflect . vecTranspose
 
 readPicture :: String -> Picture
-readPicture = map (map (\c -> case c of '#' -> On; '.' -> Off)) . splitOn "/" 
+readPicture = V.fromList . map (V.fromList . map (\c -> case c of '#' -> On; '.' -> Off)) . splitOn "/" 
 
 countPixels :: Picture -> Int
-countPixels = sum . map (length . filter ((==) On))
+countPixels = sum . V.map (V.length . V.filter ((==) On))
+
 
 main = do
     puzzleInput <- readFile "./data/21"
-    let rules = map (\rule -> (readPicture $ rule !! 0, readPicture $ rule !! 2)). map words . lines $ puzzleInput
+    let rules = M.fromList . map (\rule -> (readPicture $ rule !! 0, readPicture $ rule !! 2)). map words . lines $ puzzleInput
     let enhancements =  take 19 $ iterate (enhance rules) (readPicture ".#./..#/###")
     print $ map (countPixels . (!!) enhancements) [5, 18]
